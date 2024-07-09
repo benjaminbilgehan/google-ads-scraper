884a009b767c411b21eb266159da05c493d91d0d
benjaminbilgehan <bilgehanevren@gmail.com>
2024-07-09 06:24:13 +0300

    Add client-side Google search ad scraper addd port number

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
